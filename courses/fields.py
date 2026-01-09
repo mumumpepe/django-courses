a468c2f6e2e2b50e8c854ea4a463f805cdd076b3
mumumpepe <mumumpepe@DESKTOP-1SOQAI1.home>
2026-01-09 19:56:55 +0300

    first commit

---
+++ courses/fields.py
@@ -0,0 +1,24 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+
+
+class OverField(models.PositiveIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        self.for_fields = for_fields
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        if getattr(model_instance, self.attname) is None:
+            # no current value
+            try:
+                qs = self.model.objects.all()
+                if self.for_fields:
+                    # filter by objects with the same field values
+                    # for the fields in "for_fields"
+                    query = {
+                        field: getattr(model_instance, field)
+                        for field in self.for_fields
+                    }
+                    qs = qs.filter(**query)
+                # get the order of the last item
+                
